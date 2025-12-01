825d4a42f4381e8b1d22986ba2f8c35e86735707
Valerie Carey <vla6@cornell.edu>
2025-12-01 18:22:30 -0500

    adding name endpoint

--- test_simple/app.py
+++ test_simple/app.py
@@ -7,6 +7,18 @@ PROJECT_ID = os.environ.get("GOOGLE_CLOUD_PROJECT", "circlestar-2024")
 port = int(os.environ.get('PORT', 8080))
 
 app = Flask(__name__)
+
+@app.route('/hello/<string:name>', methods=['GET'])
+def hello_name(name):
+    """
+    Returns a JSON response greeting the name provided in the URL path.
+    Example: /hello/Billy returns {"message": "Hello, Billy"}
+    """
+    # Capitalize the name just to make the output a bit cleaner
+    greeting = f"Hello, {name.capitalize()}"
+    
+    # Return the greeting as a JSON object
+    return jsonify({"message": greeting})
                                  
 @app.route('/hello_world', methods=['GET'])
 def hello_world():
