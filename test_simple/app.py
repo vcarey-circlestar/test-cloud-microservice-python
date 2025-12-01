af9e77a9ea8106c6bdb335bb2e46bc8fe0f04df0
Valerie Carey <vla6@cornell.edu>
2025-12-01 15:42:45 -0500

    Adding service

---
+++ test_simple/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request
+
+import os, json, io, tempfile
+
+PROJECT_ID = os.environ.get("GOOGLE_CLOUD_PROJECT", "circlestar-2024")
+
+                                 
+@app.route('/hello_world', methods=['GET'])
+def hello_world():
+    return "hello world"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=DB_PORT)
