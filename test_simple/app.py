b29959304d3e6811e0ba6b24ebcf6f3b5b8f5c76
Valerie Carey <vla6@cornell.edu>
2025-12-01 16:04:21 -0500

    added port field

--- test_simple/app.py
+++ test_simple/app.py
@@ -4,7 +4,7 @@ import os, json, io, tempfile
 
 PROJECT_ID = os.environ.get("GOOGLE_CLOUD_PROJECT", "circlestar-2024")
 
-
+port = int(os.environ.get('PORT', 8080))
                                  
 @app.route('/hello_world', methods=['GET'])
 def hello_world():
@@ -12,4 +12,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=port)
